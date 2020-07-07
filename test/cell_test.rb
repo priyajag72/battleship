2931f704061a695f41c07ec8f0d075132566f277
Arique Aguilar <arique1104@gmail.com>
2020-07-07 09:13:46 -0500

    Test it exists code

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -5,9 +5,10 @@ require "./lib/cell"
 
 class CellTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
 
   #def test_it_has_attributes
   #end
