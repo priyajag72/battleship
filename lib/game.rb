8fe0183b27965eb3fff320aebb5d0d265f1389e6
Arique Aguilar <arique1104@gmail.com>
2020-07-10 20:57:35 -0500

    Add player files

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,58 @@
+require "./lib/ship"
+require "./lib/board"
+require "./lib/player"
+
 class Game
  #attr_reader
-  #def initialize(*arg)
+  def initialize
+    main_menu
+  end
 
-  #end
+  def main_menu
+    p "Welcome to BATTLESHIP"
+    p "Enter p to play. Enter q to quit."
+    user_input = gets.chomp.downcase
+    validate_user_input(user_input)
+
+  end
+
+  def validate_user_input(user_input)
+    if user_input == "p"
+      ship_placement
+    elsif user_input == "q"
+      exit
+    elsif user_input != "q" || "p"
+      print "Invalid entry, please enter p to play or q to quit."
+      second_attempt = gets.chomp.downcase
+        if second_attempt == "p"
+          ship_placement
+        elsif second_attempt == "q"
+          exit
+        elsif second_attempt != "q" || "p"
+          print "Final Attempt. Invalid entry, please enter p to play or q to quit."
+          final_attempt = gets.chomp.downcase
+            if final_attempt != "q" || "p"
+              2000.times do
+                print "You don't deserve to play"
+              end
+              exit
+            elsif final_attempt == "p"
+              ship_placement
+            else
+              exit
+            end
+        end
+    else
+      print "You have entered a restricted zone in our Battleship Game. Self-Destruct Initialized."
+      exit
+    end
+
+  end
+
+  def ship_placement
+
+
+  end
+
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -3,11 +3,17 @@ require "minitest/pride"
 require "./lib/ship"
 require "./lib/board"
 require "./lib/game"
+require "./lib/player"
 
 class GameTest < Minitest::Test
 
   def test_it_exists
     game = Game.new
+  end
+
+  def test_it_has_a_main_menu
+    assert_equal true, game.main_menu
+
   end
 
 
