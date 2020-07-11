6568d6b0ee7a87c1924d1e2525b4bf1ab41981ce
Priya Power <priyajag72@gmail.com>
2020-07-11 13:44:56 -0600

    Require files passing.

---
+++ test/player_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/player"
+
+class PlayerName < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
