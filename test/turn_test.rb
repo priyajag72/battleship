ecbd9e79421a65526fd034bfc99a197aa48b490b
Priya Power <priyajag72@gmail.com>
2020-07-12 08:29:10 -0600

    Brainstorm for behaviors of turn written into test comment lines.

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -29,4 +29,18 @@ class TurnTest < Minitest::Test
     assert_equal @player2, @turn_user.player
   end
 
+  # when turn called, it should print to terminal :auto board (render default false) and :user (render paramater set to true)
+
+  # AFTER print to terminal occurs (this looop behavior could occur in game "start" loop)
+
+  # user input of coord (singular)
+    # GUARD: user error input of more than one coord or invalid coord
+    # auto input of coord created from this?
+
+  # shoot behavior (results in hit, miss, or sunk)
+
+  # print result of both :auto and :user
+
+  # MUST RECORD RESULT TO BOARD FOR NEW RENDER (in next turn)
+
 end
