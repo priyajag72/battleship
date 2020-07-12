29c7a1b3d890af729376b9d447543499ae890bea
Priya Power <priyajag72@gmail.com>
2020-07-12 08:16:30 -0600

    Updates test file for intial setup method.

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,8 +1,24 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "./lib/ship"
+require "./lib/board"
+require "./lib/player"
 require "./lib/turn"
 
 class TurnTest < Minitest::Test
+
+  def setup
+    @board1 = Board.new
+    @board1.generate_cells
+    @board2 = Board.new
+    @board2.generate_cells
+    @cruiser1 = Ship.new("Cruiser", 3)
+    @submarine1 = Ship.new("Submarine", 2)
+    @cruiser2 = Ship.new("Cruiser", 3)
+    @submarine2 = Ship.new("Submarine", 2)
+    @player1 = Player.new(:auto, @board1)
+    @player2 = Player.new(:user, @board2)
+  end
 
   #def test_it_exists
     #assert_instance_of <Class>,
