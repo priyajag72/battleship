9f13d7ab6b0d0b974ac85119ecccdacb6800f098
Priya Power <priyajag72@gmail.com>
2020-07-12 12:43:54 -0600

    Creates setup method.

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,6 +7,23 @@ require "./lib/game"
 
 class GameTest < Minitest::Test
 
+  def setup
+    @auto_cruiser = Ship.new("Cruiser", 3)
+    @auto_submarine = Ship.new("Submarine", 2)
+    @user_cruiser = Ship.new("Cruiser", 3)
+    @user_submarine = Ship.new("Submarine", 2)
+
+    @auto_board = Board.new
+    @auto_board.generate_cells
+    @user_board = Board.new
+    @user_board.generate_cells
+
+    @auto = Player.new(:auto, @auto_board)
+    @user = Player.new(:user, @user_board)
+
+    @game = Turn.new(@user, @auto)
+  end
+
   def test_it_exists
   end
 end
