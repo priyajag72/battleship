51dbabbf48edc33e42fea5d1ee4d45e73cd32038
Priya Power <priyajag72@gmail.com>
2020-07-13 13:38:22 -0600

    Adds code for displaying player board with appropriate ships revealed according to setup method.

--- test/game_test.rb
+++ test/game_test.rb
@@ -71,7 +71,14 @@ class GameTest < Minitest::Test
   end
 
   def test_it_will_label_player_board_for_display
+    # skip
     assert_equal "==============PLAYER BOARD==============\n", @game.message_player_board
+  end
+
+  def test_it_will_display_the_player_board_and_renders_with_revealed_ships
+    # skip
+    expected = "==============PLAYER BOARD==============\n 1 2 3 4 \nA S S S . \nB . . . . \nC . . S . \nD . . S . \n"
+    assert_equal expected, @game.message_computer_display
   end
 
   def test_it_can_shoot_for_user
