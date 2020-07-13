06deaf8d481b20856674e7c1ff89afecd1c6cd25
Priya Power <priyajag72@gmail.com>
2020-07-12 20:45:53 -0600

    Removed excess code and organized helpers for completing fire coordinate.

--- lib/game.rb
+++ lib/game.rb
@@ -12,6 +12,26 @@ class Game
   def display_board
     message_turn + message_computer_display + message_player_display
   end
+
+  def fire(declarer, coordinate)
+    if declarer == @user
+      @auto.board.cells[coordinate].fire_upon
+    elsif declarer == @auto
+      @user.board.cells[coordinate].fire_upon
+    end
+
+  end
+
+  def fire_coordinate(declarer)
+    case declarer
+    when @user
+      message_user_input
+    when @auto
+      auto_generate_single_coordinate
+    end
+
+  end
+
   # ============ HELPERS FOR DISPLAY ============
   def message_turn
     @turn_counter += 1
@@ -35,24 +55,6 @@ class Game
   end
   # ============================================
 
-  def fire(declarer, coordinate)
-    if declarer == @user
-      @auto.board.cells[coordinate].fire_upon
-    elsif declarer == @auto
-      @user.board.cells[coordinate].fire_upon
-    end
-
-  end
-
-  def fire_coordinate(declarer)
-    case declarer
-    when @user
-      message_user_input
-    when @auto
-      auto_generate_single_coordinate
-    end
-
-  end
 
   # ============ HELPERS FOR FIRE COORDINATE ============
   def message_user_input
@@ -62,8 +64,7 @@ class Game
     loop do
       message_error_user_input
       valid_user_coordinate?(@turn_coord)
-      valid_coord_fired_upon?(@turn_coord)
-    break if (valid_user_coordinate?(@turn_coord) == true) && (valid_coord_fired_upon?(@turn_coord) == true)
+    break if (valid_user_coordinate?(@turn_coord) == true)
     end
     @turn_coord
   end
@@ -78,30 +79,21 @@ class Game
       print "> "
       @turn_coord = gets.chomp[0..1]
     end
+
+    until valid_coord_fired_upon?(@turn_coord)
+    end
   end
 
   def valid_coord_fired_upon?(coordinate)
-    until @user.board.cells[@turn_coord].fired_upon?
+    if @user.board.cells[@turn_coord].fired_upon? == true
       p "MISFIRE! You are trying to fire on a cell that you have already fired upon!\nThe enemy is alerted => YOU HAVE ONE CHANCE TO CHECK YOUR BOARD AND FIRE AGAIN."
       print "> "
       @turn_coord = gets.chomp[0..1]
       require "pry"; binding.pry
       valid_user_coordinate?(@turn_coord)
+    else
+      true
     end
-    return true
-  end
-
-  def fire_valid_coordinate_user?(coordinate)
-     if @user.board.cell.fired_upon == false
-       message_error_user_input
-     else
-       @user.board.generate_local_coordinates.include?(coordinate)
-     end
-
-  end
-
-  def fire_valid_coordinate_auto?(coordinate)
-    @auto.board.cell.fired_upon == false && @auto.board.generate_local_coordinates.include?(auto_generate_single_coordinate)
   end
 
   def auto_generate_single_coordinate
